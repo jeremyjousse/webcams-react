45011b66ea3e628cc2a247160038f06d9193d0c0
Jérémy Jousse <jeremy.jousse@gmail.com>
2018-01-12 21:55:23 +0100

    feat(firebase) add firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+const firebase = require("firebase")
+// Required for side-effects
+require("firebase/firestore")
+
+var config = {
+    apiKey: 'AIzaSyBoTkXFK0CgvuMZa8fqYz_LF7khkPZsz-w',
+    authDomain: 'webcams-7ce48.firebaseapp.com',
+    databaseURL: 'https://webcams-7ce48.firebaseio.com',
+    projectId: 'webcams-7ce48',
+    storageBucket: 'webcams-7ce48.appspot.com',
+    messagingSenderId: '860664255351'
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
+
+export default db;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import db from './config/firebase';
 
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { webcams: [] };
+    }
+    componentWillMount(){
+        console.log('componentWillMount')
+        db.collection("webcams").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(`${doc.id} => ${doc.data()}`);
+                let webcam = { name: doc.data().name, id: doc.id };
+                this.setState({ webcams: [webcam].concat(this.state.webcams) });
+            });
+        });
+    }
     render() {
         return (
             <div>
                 Hello Webcams!
+                
+                <ul>
+          {
+            this.state.webcams.map(
+                webcam => <li key={webcam.name}>{webcam.name}</li>
+            )
+          }
+                    </ul>
             </div>
         )
     }
